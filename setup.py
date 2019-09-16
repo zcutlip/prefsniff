fb013008fbfa25f15f46a2d319897bd583c5c5e7
Zachary Cutlip <uid000@gmail.com>
2019-09-15 19:59:15 -0700

    add long description to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup
 about = {}
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 with open("prefsniff/__about__.py") as fp:
     exec(fp.read(), about)
 
 setup(name=about["__title__"],
       version=about["__version__"],
       description=about["__summary__"],
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url="https://github.com/zcutlip/prefsniff",
       packages=['prefsniff'],
       entry_points={
