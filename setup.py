1761736aa6659759180f9a0da6ef20e088788382
Zachary Cutlip <uid000@gmail.com>
2019-09-15 19:40:09 -0700

    setup.py installer

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+about = {}
+with open("prefsniff/__about__.py") as fp:
+    exec(fp.read(), about)
+
+setup(name=about["__title__"],
+      version=about["__version__"],
+      description=about["__summary__"],
+      url="TBD",
+      packages=['prefsniff'],
+      entry_points={
+          'console_scripts': ['prefsniff=prefsniff.prefsniff_main:main'], },
+      python_requires='>=2.7',
+      install_requires=['watchdog>=0.8.3'],
+      )
